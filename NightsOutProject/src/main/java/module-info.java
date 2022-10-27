eccdf7777b179a82b6f36960369c1e9a4f377df0
AndreaGalluzzi00 <92532663+AndreaGalluzzi00@users.noreply.github.com>
2022-10-27 13:03:15 +0200

    Deleting old proj folder

--- NightsOutProject/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.nightsoutproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.nightsoutproject to javafx.fxml;
-    exports com.example.nightsoutproject;
-}
