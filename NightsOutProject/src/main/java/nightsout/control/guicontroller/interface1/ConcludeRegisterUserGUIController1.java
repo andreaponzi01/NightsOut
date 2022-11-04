66526d40e28976a915f1ffd2ecce804251fbe402
AndreaGalluzzi00 <92532663+AndreaGalluzzi00@users.noreply.github.com>
2022-11-04 17:37:28 +0100

    Registration

--- NightsOutProject/src/main/java/nightsout/control/guicontroller/interface1/ConcludeRegisterUserGUIController1.java
+++ NightsOutProject/src/main/java/nightsout/control/guicontroller/interface1/ConcludeRegisterUserGUIController1.java
@@ -1,4 +1,23 @@
 package nightsout.control.guicontroller.interface1;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import nightsout.utils.scenes.ReplaceScene;
+
 public class ConcludeRegisterUserGUIController1 {
+
+    @FXML
+    TextField textFieldEmail, textFieldUsername, textFieldPassword;
+
+    @FXML
+    Button buttonBack, buttonSubmit;
+
+    @FXML
+    protected void backToRegister(ActionEvent actionEvent) { ReplaceScene.replaceScene(actionEvent, "/RegisterUser1.fxml"); }
+
+    @FXML
+    protected void goToWelcomePage(ActionEvent actionEvent) { ReplaceScene.replaceScene(actionEvent, "/Welcome1.fxml"); }
+
 }
--- NightsOutProject/src/main/java/nightsout/control/guicontroller/interface1/RegisterUserGUIController1.java
+++ NightsOutProject/src/main/java/nightsout/control/guicontroller/interface1/RegisterUserGUIController1.java
@@ -3,18 +3,49 @@ package nightsout.control.guicontroller.interface1;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import nightsout.utils.scenes.ReplaceScene;
+import nightsout.utils.scenes.ReplaceSceneDynamic1;
+
+import java.io.IOException;
 
 public class RegisterUserGUIController1 {
+
+    private String[] personalInfo;
+
+    private void setPersonalInfo(){
+        /*
+        String name = textFieldName.getText();
+        String address = textFieldAddress.getText();
+        String city = textFieldCity.getText();
+        String discount = textFieldDiscount.getText();
+
+        personalInfo = new String[]{name, address, city, discount};
+         */
+    }
 
     @FXML
     Button buttonNextStep, buttonBack;
 
     @FXML
-    Label labelName;
+    TextField textFieldName, textFieldSurname;
+
+    @FXML
+    RadioButton radioMale, radioFemale;
+
+    @FXML
+    DatePicker dateBirthday;
 
     @FXML
     protected void backToChoice(ActionEvent actionEvent) { ReplaceScene.replaceScene(actionEvent, "/Welcome1.fxml"); }
+
+    @FXML
+    protected void goToConcludeRegister(ActionEvent actionEvent) throws IOException {
+        setPersonalInfo();
+        ReplaceSceneDynamic1 replacer = new ReplaceSceneDynamic1();
+        replacer.switchAndSetSceneRegister(actionEvent,"/ConcludeRegisterUser1.fxml", personalInfo, "Free");
+    }
 
 }
